448e82918c9f7a4583e8ab8ad5b9d61c21a94640
PotticusMaximus <136069407+PotticusMaximus@users.noreply.github.com>
2023-11-06 12:05:52 +0000

    all exercises completed

--- index.test.js
+++ index.test.js
@@ -1,22 +1,25 @@
 // install dependencies
-const { execSync } = require('child_process');
-execSync('npm install');
-execSync('npm run seed');
+const { execSync } = require("child_process");
+execSync("npm install");
+execSync("npm run seed");
 
-const request = require("supertest")
-const { db } = require('./db/connection');
-const { Musician } = require('./models/index')
-const app = require('./src/app');
+const request = require("supertest");
+const { db } = require("./db/connection");
+const { Musician } = require("./models/index");
+const app = require("./src/app");
 const seedMusician = require("./seedData");
 
+test("Musicians endpoint", async () => {
+  // Sends request to `/bakedGoods` endpoint
+  const response = await request(app).get("/musicians");
+  expect(response.statusCode).toBe(200);
+});
 
-describe('./musicians endpoint', () => {
-    // Write your tests here
-    
-    
-
-
-
-
-    
-})
+test("Testing musicians json data", async () => {
+  // Sends request to `/bakedGoods` endpoint
+  const response = await request(app).get("/musicians");
+  const responseData = JSON.parse(response.text);
+  expect(responseData[0].name).toEqual("Mick Jagger");
+  expect(responseData[1].name).toEqual("Drake");
+  expect(responseData[2].name).toEqual("Jimi Hendrix");
+});
